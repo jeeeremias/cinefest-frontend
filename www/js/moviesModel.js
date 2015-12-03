ff3f13a621e4ea647722e09d462f1ee6b558dc0c
cesarsoares94 <cesaraugustoiv@gmail.com>
2015-12-03 15:43:28 -0200

    Integração, resolvendo o problema http, tentativa 2

---
+++ www/js/moviesModel.js
@@ -0,0 +1,7 @@
+function getFilmes(){
+  this.filmes = [
+    {nome: 'Filme 1', Autor: 'Autor1'},
+    {nome: 'Filme 2', Autor: 'Autor2'},
+    {nome: 'Filme 3', Autor: 'Autor3'},
+    {nome: 'Filme 4', Autor: 'Autor4'}];
+}
