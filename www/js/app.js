243e80d7a84178f057b7a9400becc7df6bad8ee1
cesarsoares94 <cesaraugustoiv@gmail.com>
2015-12-09 19:33:39 -0200

    layout das telas de login e cadastro

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-var app = angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
+var app = angular.module('starter', ['ionic', 'starter.controllers','services'])
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -28,13 +28,21 @@ var app = angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'
     url: '/app',
     abstract: true,
     templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
   })
+
   .state('app.login', {
     url: '/login',
     views: {
       'menuContent': {
         templateUrl: 'templates/login.html'
+      }
+    }
+  })
+  .state('app.cadastro', {
+    url: '/cadastro',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/cadastro.html'
       }
     }
   })
@@ -77,28 +85,4 @@ var app = angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'
   });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/login');
-});
-
-window.fbAsyncInit = function() {
-    FB.init({
-      appId      : 'your-app-id',
-      xfbml      : true,
-      version    : 'v2.5'
-    });
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
-app.controller('movieCtrl', function($scope, $http) {
-
-  $http.get("https://cinefest2016.herokuapp.com/filmes?limit=10&offset=0")
-    .success(function(data) {
-      $scope.lista = data;
-    });
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
-angular.module('starter.controllers', [])
+var app = angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+app.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -10,6 +10,7 @@ angular.module('starter.controllers', [])
   //});
 
   // Form data for the login modal
+  $scope.loginData = {};
 
   // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
@@ -40,21 +41,7 @@ angular.module('starter.controllers', [])
   };
 })
 
-facebookApp.controller ("LoginController",function ($scope ,$cordovaOauth, $localStorage, $location)  {
-
-     $scope.login = function ()   {
-         $cordovaOauth.facebook ("CLIENT_ID_HERE",  ["email", "read_stream", "user_website","user_location","user_relationships"]).then (function(result) {
-             $localStorage.accessToken = result.access_token;
-             $location.path ("/profile") ;
-         } ,  function (error)  {
-             alert ( "There was a problem signing in!  See the console for logs" ) ;
-             console.log(error);
-         });
-     };
-
- })
-
-.controller('PlaylistsCtrl', function($scope) {
+app.controller('PlaylistsCtrl', function($scope) {
   $scope.playlists = [
     { title: 'Reggae', id: 1 },
     { title: 'Chill', id: 2 },
@@ -65,5 +52,121 @@ facebookApp.controller ("LoginController",function ($scope ,$cordovaOauth, $loca
   ];
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
+app.controller('PlaylistCtrl', function($scope, $stateParams) {
+})
+
+app.controller('movieCtrl', function($scope, $http) {
+  console.log('baixando filmes');
+  $http.get("https://cinefest2016.herokuapp.com/filmes?limit=10&offset=0")
+    .success(function(data) {
+      $scope.lista = data;
+    });
+})
+app.controller('WelcomeCtrl', function($scope, $state, $q, UserService, $ionicLoading, $ionicPopup) {
+
+  //This is the success callback from the login method
+  var fbLoginSuccess = function(response) {
+    if (!response.authResponse){
+      fbLoginError("Cannot find the authResponse");
+      return;
+    }
+
+    var authResponse = response.authResponse;
+
+    getFacebookProfileInfo(authResponse)
+    .then(function(profileInfo) {
+      //for the purpose of this example I will store user data on local storage
+      UserService.setUser({
+        authResponse: authResponse,
+				userID: profileInfo.id,
+				name: profileInfo.name,
+				email: profileInfo.email,
+        picture : "http://graph.facebook.com/" + authResponse.userID + "/picture?type=large"
+      });
+
+      $ionicLoading.hide();
+      $state.go('app.lista_filmes');
+
+    }, function(fail){
+      //fail get profile info
+      console.log('profile info fail', fail);
+    });
+  };
+
+
+  //This is the fail callback from the login method
+  var fbLoginError = function(error){
+    console.log('fbLoginError', error);
+    $ionicLoading.hide();
+  };
+
+  //this method is to get the user profile info from the facebook api
+  var getFacebookProfileInfo = function (authResponse) {
+    var info = $q.defer();
+
+    facebookConnectPlugin.api('/me?fields=email,name&access_token=' + authResponse.accessToken, null,
+      function (response) {
+				console.log(response);
+        info.resolve(response);
+      },
+      function (response) {
+				console.log(response);
+        info.reject(response);
+      }
+    );
+    return info.promise;
+  };
+
+  //This method is executed when the user press the "Login with facebook" button
+  $scope.facebookSignIn = function() {
+    console.log('login');
+    facebookConnectPlugin.getLoginStatus(function(success){
+     if(success.status === 'connected'){
+        // the user is logged in and has authenticated your app, and response.authResponse supplies
+        // the user's ID, a valid access token, a signed request, and the time the access token
+        // and signed request each expire
+        console.log('getLoginStatus', success.status);
+
+				//check if we have our user saved
+				var user = UserService.getUser('facebook');
+
+				if(!user.userID)
+				{
+					getFacebookProfileInfo(success.authResponse)
+					.then(function(profileInfo) {
+
+						//for the purpose of this example I will store user data on local storage
+						UserService.setUser({
+							authResponse: success.authResponse,
+							userID: profileInfo.id,
+							name: profileInfo.name,
+							email: profileInfo.email,
+							picture : "http://graph.facebook.com/" + success.authResponse.userID + "/picture?type=large"
+
+						});
+
+						$state.go('app.lista_filmes');
+
+					}, function(fail){
+						//fail get profile info
+						console.log('profile info fail', fail);
+					});
+				}else{
+					$state.go('app.lista_filmes');
+				}
+
+     } else {
+        //if (success.status === 'not_authorized') the user is logged in to Facebook, but has not authenticated your app
+        //else The person is not logged into Facebook, so we're not sure if they are logged into this app or not.
+        console.log('getLoginStatus', success.status);
+
+			  $ionicLoading.show({
+          template: 'Logging in...'
+        });
+
+        //ask the permissions you need. You can learn more about FB permissions here: https://developers.facebook.com/docs/facebook-login/permissions/v2.4
+        facebookConnectPlugin.login(['email', 'public_profile'], fbLoginSuccess, fbLoginError);
+      }
+    });
+  };
 });
