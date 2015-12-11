e966b5d6697f21aab304ea4e733b83b7c332d0c7
cesarsoares94 <cesaraugustoiv@gmail.com>
2015-12-11 13:35:21 -0200

    adicionado controlador LoginCtrl, metodo que envia username e senha para o servidor

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,46 +1,5 @@
 var app = angular.module('starter.controllers', [])
 
-app.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
-
 app.controller('PlaylistsCtrl', function($scope) {
   $scope.playlists = [
     { title: 'Reggae', id: 1 },
@@ -62,7 +21,27 @@ app.controller('movieCtrl', function($scope, $http) {
       $scope.lista = data;
     });
 })
-app.controller('WelcomeCtrl', function($scope, $state, $q, UserService, $ionicLoading, $ionicPopup) {
+app.controller('LoginCtrl', function($scope, $http, $state, $q, UserService, $ionicLoading, $ionicPopup) {
+
+  $scope.data = {};
+
+  $scope.submit = function(){
+    console.log("user: " + $scope.data.username + " - PW: " + $scope.data.password);
+      var link = 'http://nikola-breznjak.com/_testings/ionicPHP/api.php';
+
+      $http.post(link, {username : $scope.data.username}).then(function (res){
+          $scope.response = res.data;
+          console.log("response:" +$scope.response);
+          alert("response: "  +$scope.response);
+          if ($scope.response){//Verificar a resposta do service
+            $state.go('app.lista_filmes'); //Método que redireciona o usuario para a pagina de lista de filmes
+          }else {
+            
+          }
+
+      });
+
+  }
 
   //This is the success callback from the login method
   var fbLoginSuccess = function(response) {
@@ -169,4 +148,19 @@ app.controller('WelcomeCtrl', function($scope, $state, $q, UserService, $ionicLo
       }
     });
   };
-});
+})
+
+.controller('RegisterCtrl', function($scope, $http) {
+    $scope.data = {};
+
+    $scope.submit = function(){
+      console.log("user: " + $scope.data.username + " - PW: " + $scope.data.password);
+        var link = 'http://nikola-breznjak.com/_testings/ionicPHP/api.php';
+
+        $http.post(link, {username : $scope.data.username}).then(function (res){
+            $scope.response = res.data;
+            console.log("response:" +$scope.response);
+            alert("response: "  +$scope.response);
+        });
+    };
+})
