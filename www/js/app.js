b80ba6a5bada7f0c34d3995155820d4d6c0aff84
Jeremias Pereira <jeremias.livre@gmail.com>
2015-12-13 22:06:54 -0200

    Implementado o infinite scroll e removido algumas coisas inuteis do codigo

--- www/js/app.js
+++ www/js/app.js
@@ -55,34 +55,6 @@ var app = angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers'
       }
     }
   })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/login');
 });
