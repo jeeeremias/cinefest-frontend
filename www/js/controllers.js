547491ef55da9e761df1f1337161a78895641399
cesarsoares94 <cesar_augustoiv@hotmail.com>
2016-01-27 20:44:56 -0200

    criando a tela de programacao

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,6 +7,11 @@
       $ionicHistory.goBack();
     };
   });
+  app.controller ('programacaoCtrl', function($scope, $ionicHistory){
+    $scope.goBack = function () {
+      $ionicHistory.goBack();
+    };
+  });
   app.controller('filmeCtrl', function($scope, $http, $ionicLoading, $cordovaToast, Filmes, Imagem, sharedProperties, $state) {
     console.log('baixando filmes');
     $scope.requestListFilmes = {pag:0, tam:5}
@@ -56,7 +61,6 @@
   });
   app.controller('MenuCtrl', function($scope, $http, $state, $q, $ionicLoading, $ionicPopup, $ionicNavBarDelegate, UserService) {
     $scope.go = function(pagina) {
-      console.log(pagina);
       $state.go(pagina);
     }
   })
@@ -104,56 +108,27 @@
       }
     }
   })
-    .controller('cardapioCtrl', function($scope, $http){
-      $scope.lanches = [
-        {nome:'PULP FICTION', ingredientes: 'PÃO DE HAMBÚRGUER, HAMBÚRGUER BOVINO, BACON EM FATIAS,CHEDDAR, ANÉIS EMPANADOS E MAIONESE ESPECIAL'},
-        {nome:'KILL BILL 1', ingredientes: 'PÃO DE GERGELIM, HAMBÚRGUER DE FRANGO, QUEIJO PRATO, CEBOLA E PIMENTÃO AO MOLHO DE SOJA, MAIONESE E ALFACE AMERICANA'},
-        {nome: 'KILL BILL 2', ingredientes: 'PÃO DE GERGELIM, HAMBÚRGUER SUÍNO, QUEIJO PRATO, CHAMPIGNON GRELHADO, MAIONESE DE WASABI E AGRIÃO'},
-        {nome: 'BASTARDOS INGLÓRIOS', ingredientes: 'PÃO DE BAGUETE, LINGUIÇA SUÍNA, QUEIJO PRATO, PURÊ DE BATATA, MAIONESE DE MOSTARDA E VINAGRETE A BASE DE REPOLHO'},
-        {nome: 'CÃES DE ALUGUEL', ingredientes: 'PÃO DE CACHORRO QUENTE, SALSICHA VEGETARIANA, MAIONESE ESPECIAL, VINAGRETE, BATATA PALHA E ALFACE AMERICANA'}
-      ]
-      $scope.massas = [
-        {nome: 'OS BOAS-VIDAS', ingredientes: 'LASANHA BOLONHESA, PRESUNTO, QUEIJO, MOLHO AO SUGO, MOLHO BRANCO, MANJERICÃO E PARMESÃO'},
-        {nome: '8½ - OITO E MEIO', ingredientes: 'LASANHA DE BRÓCOLIS, PARMESÃO, PROVOLONE, CATUPIRY, GORGONZOLA, MUSSARELA, GERGELIM E MANJERICÃO CORTADO'},
-        {nome: 'GINGER E FRED ', ingredientes: 'RONDELLI DE PRESUNTO, QUEIJO, NOZ-MOSCADA, ORÉGANO E MOLHO AO SUGO'},
-        {nome: 'A VOZ DA LUA ', ingredientes: 'RONDELLI DE FRANGO, CATUPIRY E MOLHO BRANCO'},
-        {nome: 'AMORES NA CIDADE ' , ingredientes: 'PANQUECA DE PALMITO AO MOLHO SUGO'},
-        {nome: 'ROMA ', ingredientes: 'PANQUECA DE CARNE COM BACON, AZEITONA VERDE E MOLHO AO SUGO'}
-      ]
-      $scope.doces = [
-        {nome: 'BISCOITO DE CASTANHA-DO-PARÁ (150G)'},
-        {nome: 'TORTA DE CUPUAÇU E GANACHE COM MASSA DE CASTANHA'},
-        {nome: 'TORTINHA DE MORANGO'},
-        {nome: 'TORTINHA DE LIMÃO'},
-        {nome: 'TORTA HOLANDESA'},
-        {nome: 'BOMBOM DE CUPUAÇU'}
-      ]
-      $scope.empadas = [
-        {nome: 'CABRA MARCADO PARA MORRER', ingredientes: 'EMPADA DE VATAPÁ'},
-        {nome: 'O FIM E O PRINCÍPIO', ingredientes: 'EMPADA DE PALMITO'},
-        {nome: 'JOGO DE CENA ', ingredientes: 'EMPADA DE FRANGO, CATUPIRY'},
-        {nome: 'BABILÔNIA 2000', ingredientes: 'EMPADA DE ALHO-PORÓ COM PARMESÃO'}
-      ]
-      $scope.tapiocas = [
-        {nome: 'TERRA EM TRANSE', ingredientes: 'TAPIOCA DE CARNE SECA, CEBOLA E PURÊ DE ABÓBORA'},
-        {nome: 'AMAZONAS, AMAZONAS', ingredientes: 'TAPIOCA DE QUEIJO COALHO E GOIABADA'},
-        {nome: 'A IDADE DA TERRA', ingredientes: 'TAPIOCA DE QUEIJO BRANCO, TOMATE E MANJERICÃO'},
-        {nome: 'O DRAGÃO DA MALDADE CONTRA O SANTO GUERREIRO', ingredientes: 'TAPIOCA DE CARNE MOÍDA, FEIJÃO FRADINHO E PIMENTA DEDO DE MOÇA'}
-      ]
-    })
-  .controller('progracaoCtrl', function($scope, $http){
-    $scope.programacao = [];
+    .controller('cardapioCtrl', function($scope){
 
-    $http({
-      url: endpoint + '/programacao',
-      method: 'GET'
-    }).then(function(result){
-      console.log(result.data);
-      $scope.programacao = result.data;
-    }), function(error){
-      console.log('erro' + error.data.error);
-    }
-  })
+      $scope.lanches = new getLanches().lanches;
+      $scope.massas = new getMassas().massas;
+      $scope.doces = new getDoces().doces;
+      $scope.empadas = new getEmpadas().empadas;
+      $scope.tapiocas = new getTapiocas().tapiocas;
+    })
+  .controller('progracaoCtrl', function($scope){
+    $scope.programacao = new getProgramacao().primeiroDia;
+
+    $scope.dia = 15;
+    $scope.desabilitar = true;
+
+    $scope.proximo = function(){
+      $scope.dia ++;
+      if ($scope.dia == 15){
+        $scope.desabilitar = true;
+      }
+    };
+  });
     var compareTo = function()
     {
       return {
---
+++ www/js/programacaoService.js
@@ -0,0 +1,64 @@
+function getProgramacao() {
+  this.primeiroDia = [
+    {titulo: 'VII', genero: '', tempo: '', cidade: 'Pilar do Sul', estado: 'SP', hora: '20:00', local:'Bairro Vila Nova - Local: Rua Pedro Augusto Rangel', dia: 15},
+    {titulo: 'Kidchup', genero: 'Animação', tempo: '2m54', cidade: 'São Paulo', estado: 'SP', hora: '20:00', local:'Bairro Vila Nova - Local: Rua Pedro Augusto Rangel', dia: 15},
+    {titulo: 'Procura-se', genero: 'Ficcção', tempo: '12m35', cidade: 'Santos', estado: 'SP', hora: '20:00', local:'Bairro Vila Nova - Local: Rua Pedro Augusto Rangel', dia: 15},
+    {titulo: 'Abraço de urso', genero: 'Animação', tempo: '5min', cidade: 'Curitiba', estado: 'PR', hora: '20:00', local:'Bairro Vila Nova - Local: Rua Pedro Augusto Rangel', dia: 15},
+    {titulo: 'Facetime', genero: 'Ficcção', tempo: '8m36', cidade: 'São Paulo', estado: 'SP', hora: '20:00', local:'Bairro Vila Nova - Local: Rua Pedro Augusto Rangel', dia: 15},
+    {titulo: 'Moleque', genero: 'Ficcção', tempo: '20m27', cidade: 'Belo Horizonte', estado: 'MG', hora: '20:00',local:'Bairro Vila Nova - Local: Rua Pedro Augusto Rangel', dia: 15},
+    {titulo: 'Voustéquio', genero: 'Animação', tempo: '3m35', cidade: 'Vitória', estado: 'ES', hora: '20:00',local:'Bairro Vila Nova - Local: Rua Pedro Augusto Rangel', dia: 15},
+    {titulo: 'Canhain! Uma Aventura Sinistra', genero: 'Ficção', tempo: '20min', cidade: 'Cuiabá', estado: 'MT', hora: '20:00',local:'Bairro Vila Nova - Local: Rua Pedro Augusto Rangel', dia: 15}
+  ]
+  this.segundoDia = [
+    {titulo: 'Síria', genero: '', tempo: '', cidade: 'Votorantim', estado: 'SP', hora: '20:00', local:'Bairro Jardim Tatiana - Local: Anísio Pereira Nascimento', dia: 16},
+    {titulo: 'Kidchup', genero: 'Animação', tempo: '2m54', cidade: 'São Paulo', estado: 'SP', hora: '20:00', local:'Bairro Jardim Tatiana - Local: Anísio Pereira Nascimento', dia: 16},
+    {titulo: 'Procura-se', genero: 'Ficcção', tempo: '12m35', cidade: 'Santos', estado: 'SP', hora: '20:00', local:'Bairro Jardim Tatiana - Local: Anísio Pereira Nascimento', dia: 16},
+    {titulo: 'Abraço de urso', genero: 'Animação', tempo: '5min', cidade: 'Curitiba', estado: 'PR', hora: '20:00', local:'Bairro Jardim Tatiana - Local: Anísio Pereira Nascimento', dia: 16},
+    {titulo: 'Facetime', genero: 'Ficcção', tempo: '8m36', cidade: 'São Paulo', estado: 'SP', hora: '20:00', local:'Bairro Jardim Tatiana - Local: Anísio Pereira Nascimento', dia: 16},
+    {titulo: 'Moleque', genero: 'Ficcção', tempo: '20m27', cidade: 'Belo Horizonte', estado: 'MG', hora: '20:00',local:'Bairro Jardim Tatiana - Local: Anísio Pereira Nascimento', dia: 16},
+    {titulo: 'Voustéquio', genero: 'Animação', tempo: '3m35', cidade: 'Vitória', estado: 'ES', hora: '20:00',local:'Bairro Jardim Tatiana - Local: Anísio Pereira Nascimento', dia: 16},
+    {titulo: 'Canhain! Uma Aventura Sinistra', genero: 'Ficção', tempo: '20min', cidade: 'Cuiabá', estado: 'MT', hora: '20:00',local:'Bairro Jardim Tatiana - Local: Anísio Pereira Nascimento', dia: 16}
+  ]
+  this.terceiroDia = [
+    {titulo: 'Sanatório', genero: '', tempo: '', cidade: 'Piedade', estado: 'SP', hora: '20:00', local:'Bairro Green Valley - Local: Maria Rosa dos Santos', dia: 17},
+    {titulo: 'Kidchup', genero: 'Animação', tempo: '2m54', cidade: 'São Paulo', estado: 'SP', hora: '20:00', local:'Bairro Green Valley - Local: Maria Rosa dos Santos', dia: 17},
+    {titulo: 'Procura-se', genero: 'Ficcção', tempo: '12m35', cidade: 'Santos', estado: 'SP', hora: '20:00', local:'Bairro Green Valley - Local: Maria Rosa dos Santos', dia: 17},
+    {titulo: 'Abraço de urso', genero: 'Animação', tempo: '5min', cidade: 'Curitiba', estado: 'PR', hora: '20:00', local:'Bairro Green Valley - Local: Maria Rosa dos Santos', dia: 17},
+    {titulo: 'Facetime', genero: 'Ficcção', tempo: '8m36', cidade: 'São Paulo', estado: 'SP', hora: '20:00', local:'Bairro Green Valley - Local: Maria Rosa dos Santos', dia: 17},
+    {titulo: 'Moleque', genero: 'Ficcção', tempo: '20m27', cidade: 'Belo Horizonte', estado: 'MG', hora: '20:00',local:'Bairro Green Valley - Local: Maria Rosa dos Santos', dia: 17},
+    {titulo: 'Voustéquio', genero: 'Animação', tempo: '3m35', cidade: 'Vitória', estado: 'ES', hora: '20:00',local:'Bairro Green Valley - Local: Maria Rosa dos Santos', dia: 17},
+    {titulo: 'Canhain! Uma Aventura Sinistra', genero: 'Ficção', tempo: '20min', cidade: 'Cuiabá', estado: 'MT', hora: '20:00',local:'Bairro Green Valley - Local: Maria Rosa dos Santos', dia: 17}
+  ]
+  this.quartoDia = [
+    {titulo: 'Meninas do Paquistão', genero: '', tempo: '', cidade: 'Votorantim', estado: 'SP', hora: '20:00', local:'Bairro Itapeva - Local: Maria José Silveira Alves', dia: 18},
+    {titulo: 'Kidchup', genero: 'Animação', tempo: '2m54', cidade: 'São Paulo', estado: 'SP', hora: '20:00', local:'Bairro Itapeva - Local: Maria José Silveira Alves', dia: 18},
+    {titulo: 'Procura-se', genero: 'Ficcção', tempo: '12m35', cidade: 'Santos', estado: 'SP', hora: '20:00', local:'Bairro Itapeva - Local: Maria José Silveira Alves', dia: 18},
+    {titulo: 'Abraço de urso', genero: 'Animação', tempo: '5min', cidade: 'Curitiba', estado: 'PR', hora: '20:00', local:'Bairro Itapeva - Local: Maria José Silveira Alves', dia: 18},
+    {titulo: 'Facetime', genero: 'Ficcção', tempo: '8m36', cidade: 'São Paulo', estado: 'SP', hora: '20:00', local:'Bairro Bairro Itapeva - Local: Maria José Silveira Alves', dia: 18},
+    {titulo: 'Moleque', genero: 'Ficcção', tempo: '20m27', cidade: 'Belo Horizonte', estado: 'MG', hora: '20:00',local:'Bairro Itapeva - Local: Maria José Silveira Alves', dia: 18},
+    {titulo: 'Voustéquio', genero: 'Animação', tempo: '3m35', cidade: 'Vitória', estado: 'ES', hora: '20:00',local:'Bairro Itapeva - Local: Maria José Silveira Alves', dia: 18},
+    {titulo: 'Canhain! Uma Aventura Sinistra', genero: 'Ficção', tempo: '20min', cidade: 'Cuiabá', estado: 'MT', hora: '20:00',local:'Bairro Itapeva - Local: Maria José Silveira Alves', dia: 18}
+  ]
+  this.quintoDia = [
+    {titulo: 'Atalhos', genero: '', tempo: '', cidade: 'Pilar do Sul', estado: 'SP', hora: '20:00', local:'Bairro São Lucas - Local: José Maria de Barros', dia: 18},
+    {titulo: 'Kidchup', genero: 'ANIMAÇÃO', tempo: '2m54', cidade: 'São Paulo', estado: 'SP', hora: '20:00', local:'Bairro São Lucas - Local: José Maria de Barros', dia: 19},
+    {titulo: 'Procura-se', genero: 'Ficcção', tempo: '12m35', cidade: 'Santos', estado: 'SP', hora: '20:00', local:'Bairro São Lucas - Local: José Maria de Barros', dia: 19},
+    {titulo: 'Abraço de urso', genero: 'Animação', tempo: '5min', cidade: 'Curitiba', estado: 'PR', hora: '20:00', local:'Bairro São Lucas - Local: José Maria de Barros', dia: 19},
+    {titulo: 'Facetime', genero: 'Ficcção', tempo: '8m36', cidade: 'São Paulo', estado: 'SP', hora: '20:00', local:'Bairro São Lucas - Local: José Maria de Barros', dia: 19},
+    {titulo: 'Moleque', genero: 'Ficcção', tempo: '20m27', cidade: 'Belo Horizonte', estado: 'MG', hora: '20:00',local:'Bairro São Lucas - Local: José Maria de Barros', dia: 19},
+    {titulo: 'Voustéquio', genero: 'Animação', tempo: '3m35', cidade: 'Vitória', estado: 'ES', hora: '20:00',local:'Bairro São Lucas - Local: José Maria de Barros', dia: 19},
+    {titulo: 'Canhain! Uma Aventura Sinistra', genero: 'Ficção', tempo: '20min', cidade: 'Cuiabá', estado: 'MT', hora: '20:00',local:'Bairro São Lucas - Local: José Maria de Barros', dia: 19}
+  ]
+  this.sextoDia = [
+    {titulo: 'Salve nossas Almas', genero: '', tempo: '', cidade: 'Salto de Pirapora', estado: 'SP', hora: '19:00', local:'Teatro Municipal Francisco Beranger – Votorantim', dia: 20},
+    {titulo: 'Zé Franco - Rei dos Fumos', genero: 'Documentário', tempo: '10 min', cidade: 'Sorocaba', estado: 'SP', hora: '20:00', local:'Teatro Municipal Francisco Beranger – Votorantim', dia: 20},
+    {titulo: 'Mestres do Overground', genero: 'Ficcção', tempo: '6m55', cidade: 'Sorocaba', estado: 'SP', hora: '20:00', local:'Teatro Municipal Francisco Beranger – Votorantim', dia: 20},
+    {titulo: 'Lacunas', genero: 'Animação', tempo: '14m44', cidade: 'Sorocaba', estado: 'SP', hora: '20:00', local:'Teatro Municipal Francisco Beranger – Votorantim', dia: 20},
+    {titulo: 'Espera', genero: 'Ficção', tempo: '7m45', cidade: 'São Paulo', estado: 'SP', hora: '21:00', local:'Teatro Municipal Francisco Beranger – Votorantim', dia: 20},
+    {titulo: 'Essa Moça,,,', genero: 'Animação', tempo: '1min', cidade: 'São Paulo', estado: 'SP', hora: '21:00', local:'Teatro Municipal Francisco Beranger – Votorantim', dia: 20},
+    {titulo: 'Réquiem Meneghetti', genero: 'Ficcção', tempo: '8m10', cidade: 'Rio de Janeiro', estado: 'RJ', hora: '21:00', local:'Teatro Municipal Francisco Beranger – Votorantim', dia: 20},
+    {titulo: 'E o Amor Foi Se Tornando Cada Dia Mais Distante', genero: 'Documentário', tempo: '9 min', cidade: 'Rio de Janeiro', estado: 'RJ', hora: '21:00', local:'Teatro Municipal Francisco Beranger – Votorantim', dia: 20},
+    {titulo: 'O Melhor Som do Mundo', genero: 'Ficcção', tempo: '13 min', cidade: 'São Paulo', estado: 'SP', hora: '21:00', local:'Teatro Municipal Francisco Beranger – Votorantim', dia: 20},
+    {titulo: 'O Golpe', genero: 'Documentário', tempo: '17m20', cidade: 'São Paulo', estado: 'SP', hora: '21:00', local:'Teatro Municipal Francisco Beranger – Votorantim', dia: 20}
+  ]
+}
