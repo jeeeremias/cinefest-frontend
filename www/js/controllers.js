49f87e1e948cff2bde8fba16b11ece286f073510
cesarsoares94 <cesar_augustoiv@hotmail.com>
2016-01-30 20:19:58 -0200

    programacao atualizada

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,152 +1,153 @@
-  var app = angular.module('cinefestApp.controllers', []);
-  var endpoint = 'http://rest-cinefest.rhcloud.com';
+var app = angular.module('cinefestApp.controllers', []);
+var endpoint = 'http://rest-cinefest.rhcloud.com';
 //  var endpoint = 'http://localhost:8080';
 
-  app.controller ('barCtrl', function($scope, $ionicHistory){
-    $scope.goBack = function () {
-      $ionicHistory.goBack();
-    };
-  });
-  app.controller ('programacaoCtrl', function($scope, $ionicHistory){
-    $scope.goBack = function () {
-      $ionicHistory.goBack();
-    };
-  });
-  app.controller('filmeCtrl', function($scope, $http, $ionicLoading, $cordovaToast, Filmes, Imagem, sharedProperties, $state) {
-    console.log('baixando filmes');
-    $scope.requestListFilmes = {pag:0, tam:5}
-    $scope.lista = [];
-    $scope.noMoreItemsAvailable = false;
-    $scope.loadMore = function() {
-      $http({
-        url: endpoint + '/filmes',
-        method: "GET",
-        params: $scope.requestListFilmes
-      }).then(function(result) {
-        if (result.data.length == 0) {
-          $scope.noMoreItemsAvailable = true;
-        }
-        for (var i in result.data) {
-          $scope.lista.push(result.data[i]);
-        }
-        console.log($scope.lista);
-        $scope.$broadcast('scroll.infiniteScrollComplete');
-        $scope.requestListFilmes.pag ++;
-      }, function(error){
-        $cordovaToast.showLongBottom(error.data.error);
-      });
-    };
+app.controller ('barCtrl', function ($scope, $ionicHistory) {
+  $scope.goBack = function () {
+    $ionicHistory.goBack();
+  };
+});
 
-    $scope.nome = function (idFilme) {
-      sharedProperties.setProperty(idFilme);
-      console.log('id passado' + idFilme);
-      $state.go('detalhe_filme');
-    };
-    $scope.goBack = function(){
-      $ionicHistory.goBack();
-    }
-  });
-
-  app.controller('detalhesCtrl', function($scope, $http, sharedProperties){
-    $scope.id = {id:sharedProperties.getProperty()};
+app.controller('filmeCtrl', function ($scope, $http, $ionicLoading, $cordovaToast, Filmes, Imagem, sharedProperties, $state) {
+  console.log('baixando filmes');
+  $scope.requestListFilmes = {pag: 0, tam: 5}
+  $scope.lista = [];
+  $scope.noMoreItemsAvailable = false;
+  $scope.loadMore = function () {
     $http({
-      url: endpoint + '/filme',
+      url: endpoint + '/filmes',
       method: "GET",
-      params: $scope.id
-    }).then(function(result){
-      console.log(result.data);
-    }, function(error){
-      console.log('erro' + error.data.error);
+      params: $scope.requestListFilmes
+    }).then(function (result) {
+      if (result.data.length == 0) {
+        $scope.noMoreItemsAvailable = true;
+      }
+      for (var i in result.data) {
+        $scope.lista.push(result.data[i]);
+      }
+      console.log($scope.lista);
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+      $scope.requestListFilmes.pag++;
+    }, function (error) {
+      $cordovaToast.showLongBottom(error.data.error);
     });
+  };
+
+  $scope.nome = function (idFilme) {
+    sharedProperties.setProperty(idFilme);
+    console.log('id passado' + idFilme);
+    $state.go('detalhe_filme');
+  };
+  $scope.goBack = function () {
+    $ionicHistory.goBack();
+  }
+});
+
+app.controller('detalhesCtrl', function ($scope, $http, sharedProperties) {
+  $scope.id = {id: sharedProperties.getProperty()};
+  $http({
+    url: endpoint + '/filme',
+    method: "GET",
+    params: $scope.id
+  }).then(function (result) {
+    console.log(result.data);
+  }, function (error) {
+    console.log('erro' + error.data.error);
   });
-  app.controller('MenuCtrl', function($scope, $http, $state, $q, $ionicLoading, $ionicPopup, $ionicNavBarDelegate, UserService) {
-    $scope.go = function(pagina) {
+});
+app.controller('MenuCtrl', function ($scope, $http, $state, $q, $ionicLoading, $ionicPopup, $ionicNavBarDelegate, UserService) {
+    $scope.go = function (pagina) {
       $state.go(pagina);
     }
   })
 
-  .controller('RegisterCtrl', function($scope, $http, $state, $cordovaToast) {
+  .controller('RegisterCtrl', function ($scope, $http, $state, $cordovaToast) {
 
     $scope.usuario = {};
 
-    $scope.submitCadastro = function() {
+    $scope.submitCadastro = function () {
       $http({
         url: endpoint + '/cadastro',
         method: "POST",
         data: $scope.usuario
-      }).then(function(result) {
+      }).then(function (result) {
         if (result.data.sucesso) {
           console.log(result.data.mensagem);
           $state.go('login');
         } else {
           $cordovaToast.showLongBottom(result.data.mensagem);
         }
-      }, function(error){
+      }, function (error) {
         $cordovaToast.showLongBottom(error.data.error);
       });
     }
   })
-  .controller('votoCtrl', function($scope, $http){
+  .controller('votoCtrl', function ($scope, $http) {
     $scope.lista = [];
-    $scope.requestListFilmes = {pag:0, tam:10}
+    $scope.requestListFilmes = {pag: 0, tam: 10}
     $http({
       url: endpoint + '/filmes',
       method: 'GET',
       params: $scope.requestListFilmes
-    }).then(function(result){
+    }).then(function (result) {
       console.log(result.data);
       $scope.lista = result.data;
-    }), function(error){
+    }), function (error) {
       console.log('erro' + error.data.error);
     }
 
     $scope.voto = {};
-    $scope.submitForm = function(){
+    $scope.submitForm = function () {
       $scope.submitted = true;
-      if ($scope.submitted == true){
+      if ($scope.submitted == true) {
         console.log("Clicou no botão confirmar");
       }
     }
   })
-    .controller('cardapioCtrl', function($scope){
+  .controller('cardapioCtrl', function ($scope) {
 
-      $scope.lanches = new getLanches().lanches;
-      $scope.massas = new getMassas().massas;
-      $scope.doces = new getDoces().doces;
-      $scope.empadas = new getEmpadas().empadas;
-      $scope.tapiocas = new getTapiocas().tapiocas;
-    })
-  .controller('progracaoCtrl', function($scope){
+    $scope.lanches = new getLanches().lanches;
+    $scope.massas = new getMassas().massas;
+    $scope.doces = new getDoces().doces;
+    $scope.empadas = new getEmpadas().empadas;
+    $scope.tapiocas = new getTapiocas().tapiocas;
+  })
+  .controller('progracaoCtrl', function ($scope, $timeout) {
     $scope.programacao = new getProgramacao().primeiroDia;
 
     $scope.dia = 15;
-    $scope.desabilitar = true;
 
-    $scope.proximo = function(){
+    $scope.desabilitar_proximo = false;
+
+    if ($scope.dia == 16) {
+      $scope.desabilitar_anterior = true;
+    }
+
+    $timeout(function () {
+
+    }, 1000);
+
+
+    $scope.proximo = function () {
       $scope.dia ++;
-      if ($scope.dia == 15){
-        $scope.desabilitar = true;
-      }
+    };
+    $scope.anterior = function () {
+      $scope.dia--;
     };
   });
-    var compareTo = function()
-    {
-      return {
-        require: "ngModel", scope:
-        {
-          otherModelValue: "=compareTo"
-      },
-      link: function(scope, element, attributes, ngModel)
-      {
-        ngModel.$validators.compareTo = function(modelValue)
-        {
-          return modelValue == scope.otherModelValue;
-        };
-        scope.$watch("otherModelValue", function()
-        {
-          ngModel.$validate();
-        });
-      }
-    };
+var compareTo = function () {
+  return {
+    require: "ngModel", scope: {
+      otherModelValue: "=compareTo"
+    },
+    link: function (scope, element, attributes, ngModel) {
+      ngModel.$validators.compareTo = function (modelValue) {
+        return modelValue == scope.otherModelValue;
+      };
+      scope.$watch("otherModelValue", function () {
+        ngModel.$validate();
+      });
+    }
   };
-  app.directive("compareTo", compareTo);
+};
+app.directive("compareTo", compareTo);
